6ca0722b34668da28cfa118269d81036334dfcb4
cosmos-1885 <dmsrkql104321@gmail.com>
2023-05-01 20:08:43 +0900

    fix: fix cow function

--- cow.py
+++ cow.py
@@ -1,7 +1,6 @@
 def cow(randomNum, inputNum):
-    cowCount = 0
+    count = 0
     for num in inputNum:
         if num in randomNum:
-            cowCount += 1
-            randomNum.remove(num)
-    return cowCount
+            count += 1
+    return count
