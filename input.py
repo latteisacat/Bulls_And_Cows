1c4636ddcb83964aebe5e7f557fad9a675ec2ead
cosmos-1885 <dmsrkql104321@gmail.com>
2023-05-01 20:26:33 +0900

    feat: Create input

---
+++ input.py
@@ -0,0 +1,14 @@
+import bulls
+import bulls_and_cows_answer
+import cow
+
+randomNum = bulls_and_cows_answer.create_answer()
+
+guessNum = list(input("Guess your number": ))
+
+bullsCount = bulls.bulls()
+cowsCount = cow.cow()
+
+print(f'bulls: {bullsCount} cows: {cowsCount}')
+
+
